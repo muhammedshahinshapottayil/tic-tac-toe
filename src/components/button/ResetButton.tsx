92853c8ceee638fe5d58a3e5034858ec37240c94
muhammedshahinshapottayil <muhammedshahinshapottayil@gmail.com>
2024-05-10 12:31:48 +0530

    button padding updated

--- src/components/button/ResetButton.tsx
+++ src/components/button/ResetButton.tsx
@@ -4,7 +4,7 @@ function ResetButton({ resetGame, text, isReset }: ResetButtonType) {
   return (
     <button
       className={`${
-        isReset ? "ml-2 px-2 py-1" : "px-2 py-1 "
+        isReset ? "ml-2 px-2 py-1" : "px-4 py-2 "
       } font-semibold bg-indigo-500 text-white rounded-md shadow-md hover:bg-indigo-600 transition-colors duration-300`}
       onClick={resetGame}
     >
